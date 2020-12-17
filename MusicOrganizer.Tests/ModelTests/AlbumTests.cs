fdf4545850930799cb3eec65e721351f014cecbc
Constantine <faustlarsen@gmail.com>
2020-12-17 11:42:29 -0700

    pass test return names of the album
    Co-authored-by: Garrett Dean Brown <gman9mm@live.com>

--- MusicOrganizer.Tests/ModelTests/AlbumTests.cs
+++ MusicOrganizer.Tests/ModelTests/AlbumTests.cs
@@ -19,5 +19,21 @@ namespace MusicOrganizer.Tests
     Album newAlbum = new Album("test", "test", "test");
     Assert.AreEqual(typeof(Album), newAlbum.GetType());
     }
+    [TestMethod]
+    public void GetName_ReturnsNameString_String()
+    {
+      string albumName = "Vacancy";
+      string artist = "Four Seconds Ago";
+      string genre = "Lo-Fi";
+
+      Album newAlbum = new Album(albumName, artist, genre);
+      string result1 = newAlbum.AlbumName;
+      string result2 = newAlbum.Artist;
+      string result3 = newAlbum.Genre;
+    
+      Assert.AreEqual(albumName, result1);
+      Assert.AreEqual(artist, result2);
+      Assert.AreEqual(genre, result3);
+    }
   }
 }
